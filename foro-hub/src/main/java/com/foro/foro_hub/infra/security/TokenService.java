80e4924653c2606b6c7254089d1b8210b9c4b021
Victor Reyna <73621023+alemakey@users.noreply.github.com>
2026-02-24 23:40:54 -0600

    Agrega TokenService para generar y validar JWT con Auth0

---
+++ foro-hub/src/main/java/com/foro/foro_hub/infra/security/TokenService.java
@@ -0,0 +1,52 @@
+package com.foro.foro_hub.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.foro.foro_hub.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.secret}")
+    private String apiSecret;
+
+    public String generarToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.create()
+                    .withIssuer("foro-hub")
+                    .withSubject(usuario.getUsername())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algorithm);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error al generar token JWT: " + exception.getMessage());
+        }
+    }
+
+    public String getSubject(String token) {
+        if (token == null) {
+            throw new RuntimeException("Token nulo");
+        }
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.require(algorithm)
+                    .withIssuer("foro-hub")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT invalido: " + exception.getMessage());
+        }
+    }
+
+    private Instant generarFechaExpiracion() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+    }
+}
